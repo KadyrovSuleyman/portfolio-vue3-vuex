4e2a5f9c9458e9069315170527481d10dfa0b012
Кадыров Сулейман <kadyrov.suleyman@yandex.ru>
2022-03-22 18:59:32 +0300

    начало

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,24 @@
-import { createStore } from 'vuex';
+import { createLogger, createStore } from 'vuex';
+import types from './mutation-types';
+
+const debug = process.env.NODE_ENV !== 'production';
 
 export default createStore({
   state: {
+    count: 1,
   },
   getters: {
   },
   mutations: {
+    [types.INCREMENT]: (state, n = 1) => { state.count += n; },
   },
   actions: {
+    [types.INCREMENT]: ({ commit }, n: number) => setTimeout(() => {
+      commit(types.INCREMENT, n);
+    }, 1000),
   },
   modules: {
   },
+
+  plugins: [createLogger()],
 });
